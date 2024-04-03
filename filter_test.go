fc735b5a6f92cb66aa550d8e57837eb85fb00fc3
winebarrel <sugawara@winebarrel.jp>
2024-04-03 19:24:08 +0900

    Add TestFilter_MultiFiles()

--- filter_test.go
+++ filter_test.go
@@ -45,3 +45,65 @@ zoo
 baz
 `, string(out))
 }
+
+func TestFilter_MultiFiles(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	fin1, _ := os.CreateTemp("", "")
+	defer os.Remove(fin1.Name())
+	fin1.WriteString(`
+
+foo
+bar
+
+zoo
+
+
+baz
+
+
+`)
+	fin1.Sync()
+
+	fin2, _ := os.CreateTemp("", "")
+	defer os.Remove(fin2.Name())
+	fin2.WriteString(`
+
+ xfoo
+ xbar
+
+ xzoo
+
+
+ xbaz
+
+
+`)
+	fin2.Sync()
+
+	options := &kuu.Options{
+		Inplace: true,
+	}
+
+	err := kuu.Filter([]string{fin1.Name(), fin2.Name()}, options)
+	require.NoError(err)
+
+	out1, _ := os.ReadFile(fin1.Name())
+	assert.Equal(`foo
+bar
+
+zoo
+
+baz
+`, string(out1))
+
+	out2, _ := os.ReadFile(fin2.Name())
+	assert.Equal(` xfoo
+ xbar
+
+ xzoo
+
+ xbaz
+`, string(out2))
+}
